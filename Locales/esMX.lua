be8ff262fea561c8e68175cec522106749368ea6
Kyle Scheuing <oppzippy@gmail.com>
2019-04-03 05:18:43 -0400

    Add locale files

--- Locales/esMX.lua
+++
@@ -1,9 +0,0 @@
-local L = LibStub("AceLocale-3.0"):NewLocale("TankMD", "esMX")
-if not L then return end
-
-L.title = "TankMD"
-L.toFirst = "%s al primer %s"
-L.toSecond = "%s al segundo %s"
-L.TANK = _G['TANK']
-L.HEALER = _G['HEALER']
-L.DAMAGER = _G['DAMAGER']
---
+++ Locales/ptBR.lua
@@ -0,0 +1,8 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("TankMD", "ptBR")
+
+-- L.title = "TankMD"
+-- L.toFirst = "%s to first %s"
+-- L.toSecond = "%s to second %s"
+L.TANK = _G['TANK']
+L.HEALER = _G['HEALER']
+L.DAMAGER = _G['DAMAGER']
