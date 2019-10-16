96f3468fa1033df174b5dac30451fff67fa7753e
Kyle Scheuing <oppzippy@gmail.com>
2019-10-16 01:21:11 -0400

    Remove esMX

--- Locales/esES.lua
+++ Locales/esES.lua
@@ -1,4 +1,4 @@
-local L = LibStub("AceLocale-3.0"):NewLocale("TankMD", "esES")
+local L = LibStub("AceLocale-3.0"):NewLocale("TankMD", "esES") or LibStub("AceLocale-3.0"):NewLocale("TankMD", "esMX")
 if not L then return end
 
 -- Blizzard translations
--- Locales/esMX.lua
+++
@@ -1,9 +0,0 @@
-local L = LibStub("AceLocale-3.0"):NewLocale("TankMD", "esMX")
-if not L then return end
-
--- Blizzard translations
-L.TANK = _G['TANK']
-L.HEALER = _G['HEALER']
-L.DAMAGER = _G['DAMAGER']
-
---@localization(locale="esMX", handle-unlocalized="comment")@
